d2f8fa3d4175429301dc8df2dbfbfb36809fe896
akun <victorsos@163.com>
2014-03-09 18:16:13 +0800

    更换样式为Trac风格

--- conf.py
+++ conf.py
@@ -88,6 +88,7 @@ pygments_style = 'sphinx'
 
 
 # -- Options for HTML output ---------------------------------------------------
+html_style = 'tracsphinx.css'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
